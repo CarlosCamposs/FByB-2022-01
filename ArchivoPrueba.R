1823a093b0196b690298223de0d823fa9231ddb8
minibeto666 <64914014+minibeto666@users.noreply.github.com>
2022-05-12 18:43:21 -0500

    Update ArchivoPrueba.R
    8====D

--- ArchivoPrueba.R
+++ ArchivoPrueba.R
@@ -1,50 +1,94 @@
-
-#### PROYECTO 3 FByB 
-
 # shiny test
 
 library(shiny)
 library(shinythemes)
+library(xts)
 library(TTR)
 library(quantmod)
-
+#------------------------------------------------------------------------------
 #definimos el ui para la app
 
- ui<-fluidPage(
-  
-   titlePanel("Análisis Técnico en R"),
-   
-   sidebarLayout(position = "left", 
-     sidebarPanel("Elegir acción e indicadores a visualizar",
-     
-     selectInput(inputId = "stock", 
-                 label = "Elegir acción:",
-                 choices = c("NKE","F","TSLA", "WOOF"), selected = "NKE"),
-     
-     checkboxGroupInput(inputId = "ind", label = "Escoger indicadores a visualizar", choices = c("SMA","EMA","RSI","MACD","ATR","BB") )
-     ),
-     
-     mainPanel("Mostramos gráficas",
-      
-      textOutput("selected_stock")
-                        
-               )
-     )
-   )
-  
+ui<-fluidPage( theme = shinytheme("flatly"),
+               
+               titlePanel("Análisis Técnico en R"),
+               
+               sidebarLayout(position = "left", 
+                             sidebarPanel(
+                               
+                               selectInput(inputId = "Stock", 
+                                           label = h2("Elegir acción:"),
+                                           choices = c("NKE","F","TSLA", "WOOF"), selected = "NKE"),#selectinput
+                               
+                               radioButtons(inputId="period", label=h4("Periodicidad"), 
+                                            choices=c("Diario","Semanal","Mensual") ,selected = "Diario"),#radiobutton1
+                               
+                               radioButtons(inputId="timeframe", label=h4("Periodo de tiempo"), 
+                                            choices=c("Este año","Últimos 3 meses","Últimos 6 meses",
+                                                      "Último año","Últimos 3 años","Últimos 5 años",
+                                                      "Últimos 10 años"),selected = "Este año"),#radiobutton2
+                               
+                               checkboxGroupInput(inputId = "ind", label = h4("Escoger indicadores a visualizar"), choices = c("VOL","SMA","EMA","RSI","MACD","ATR","BB") )
+                             ), #sidebar 
+                             
+                             mainPanel(
+                               
+                               h1( textOutput("selected_Stock") ), #header interactua con server
+                               plotOutput(outputId = "grafico") #grafica que interactua con server (grafico velas)
+                               
+                             ) #mainpanel
+               )#sidebarlayout
+)#fluid page
+#-------------------------------------------------------------------------------------
 # hacemos parte tecnica de server
+
+server<-function(input,output){
+   
+  output$selected_Stock <- renderText({ 
+    paste("Gráfico ",input$period," de $", input$Stock) #cambia el header
+  })
   
- server<-function(input,output){
-   
-   output$selected_stock <- renderText({ 
-     paste("Gráfico de ", input$stock)
-   })
-   
-   
-   
- }
+   dataInput <- reactive({
+     
+     
+     if(input$period == "Diario"){
+       pe<-"daily"
+     }else if(input$period == "Semanal"){
+       pe<-"weekly"
+     }else if(input$period == "Mensual"){
+       pe<-"monthly"
+     }else{
+       pe<-NA
+     } # if para determinar el formato de la serie de datos por periodicidad
+     
+    getSymbols(input$Stock, src = "yahoo",periodicity=pe,
+               auto.assign = FALSE)
+  })
+  
  
- #corre la app
- 
- shinyApp(ui = ui, server = server)
- 
+  
+  output$grafico <- renderPlot({
+  
+    if(input$timeframe == "Este año"){
+      tf<-"last 1 years"
+    }else if(input$timeframe == "Últimos 3 años"){
+      tf<-"last 3 years"
+    }else if(input$timeframe == "Últimos 5 años"){
+      tf<-"last 5 years"
+    }else if(input$timeframe == "Últimos 3 meses"){
+      tf<-"last 3 months"
+    }else if(input$timeframe == "Últimos 6 meses"){
+      tf<-"last 6 months"
+    }else if(input$timeframe == "Últimos 10 años"){
+      tf<-"last 10 years"
+    }else{ tf<- "2022"}
+    
+    chartSeries( dataInput() ,typle="candle",
+                 subset = tf ,theme=chartTheme("white",up.col="Green",dn.col="Red"),name = input$Stock ,TA=NULL) #la madre del env stock jala los datos
+    
+  },width = 1200 ,height = 800 ) 
+  
+}
+
+#corre la app
+
+shinyApp(ui = ui, server = server)
